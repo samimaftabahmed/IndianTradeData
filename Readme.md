f6f042a2f8d4367abcfa984989a37bb66a7201ed
Samim <samimaftabahmed@gmail.com>
2019-09-23 13:17:26 +0530

    First Commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Indian Trade Data Analysis
+
+Map Reduce Program to analyse trade statistics of India for export and import of commodities.
+
+For dataset visit: https://www.kaggle.com/lakshyaag/india-trade-data
---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class App extends Configured implements Tool {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+    @Override
+    public int run(String[] args) throws Exception {
+        return 0;
+    }
+}
